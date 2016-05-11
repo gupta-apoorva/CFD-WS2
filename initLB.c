7da1cb8e17129c2af206a0f1d511e94899655ea6
Apoorv Gupta <apoorva.gupta@tum.de>
2016-05-11 18:59:13 +0200

    Initialized the flagfiled streamfield and collidefield

--- initLB.c
+++ initLB.c
@@ -1,5 +1,8 @@
 #include "initLB.h"
 
+int Q = 19;
+
+
 int readParameters(int *xlength, double *tau, double *velocityWall, int *timesteps, int *timestepsPerPlotting, int argc, char *arg)
 {
 
@@ -14,17 +17,100 @@ READ_INT   ( arg, *timestepsPerPlotting );
 
 else if(argc == 1)
 {
-printf ("ERROR: File Name not provided.");
+printf ("ERROR: File Name not provided.\n");
 }
 else
 {
-printf ("ERROR: Too many input arguments.");
+printf ("ERROR: Too many input arguments.\n");
 }
 return 0;
 }
 
 
-void initialiseFields(double *collideField, double *streamField, int *flagField, int xlength){
-  /* TODO */
+void initialiseFields(double *collideField, double *streamField, int *flagField, int xlength)
+{
+  int x,y,z, i;
+ 
+// Initializing the flag field
+
+
+
+  for (x =0;x<(xlength+2);x++)
+     {
+       for (y =0;y<(xlength+2);y++)
+         {
+            for (z =0;z<(xlength+2);z++)
+             {
+                flagField[z*(xlength+2)*(xlength+2) + y*(xlength+2) + x] = 1;
+             }
+         }
+      }
+                                                            // FLUID = 0, NO_SLIp = 1 , MOVING_WALL = 2
+  for (z =1;z<(xlength+1);z++)
+     {
+       for (y =1;y<(xlength+1);y++)
+         {
+            for (x =1;x<(xlength+1);x++)
+             {
+                flagField[z*(xlength+2)*(xlength+2) + y*(xlength+2) + x] = 0;
+             }
+         }
+      }
+   z = xlength+1;
+   while (z > xlength)
+     {
+       for (x =0;x<(xlength+2);x++)
+         {
+            for (y =0;y<(xlength+2);y++)
+             {
+                flagField[z*(xlength+2)*(xlength+2) + y*(xlength+2) + x] = 2;
+             }
+         }
+      --z;
+      }
+
+// Initializing the collideField
+
+  for (x =0;x<(xlength+2);x++)
+     {
+       for (y =0;y<(xlength+2);y++)
+         {
+            for (z =0;z<(xlength+2);z++)
+             {
+                 for( i =0 ; i<Q ; i++)
+                  {
+                    collideField[Q*(z*(xlength+2)*(xlength+2) + y*(xlength+2) + x) + i] = 1;
+                  }
+             }
+         }
+      }
+
+// Initializing the streamfield
+
+  for (x =0;x<(xlength+2);x++)
+     {
+       for (y =0;y<(xlength+2);y++)
+         {
+            for (z =0;z<(xlength+2);z++)
+             {
+                 for( i =0 ; i<Q ; i++)
+                  {
+                    streamField[Q*(z*(xlength+2)*(xlength+2) + y*(xlength+2) + x) + i] = 1;
+                  }
+             }
+         }
+      }
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
--- main.c
+++ main.c
@@ -8,21 +8,54 @@
 #include "boundary.h"
 
 int main(int argc, char *argv[]){
-
- int xlength;                       /* reads domain size. Parameter name: "xlength" */
+    
+    int Q = 19;
+    int xlength;                       /* reads domain size. Parameter name: "xlength" */
     double tau;                        /* relaxation parameter tau. Parameter name: "tau" */
     double velocityWall;               /* velocity of the lid. Parameter name: "characteristicvelocity" */
     int timesteps;                     /* number of timesteps. Parameter name: "timesteps" */
     int timestepsPerPlotting;
+    double* collideField; 
+    double* streamField;
+    int* flagField;
 
+    readParameters(&xlength, &tau, &velocityWall, &timesteps, &timestepsPerPlotting, argc, argv[1]);
+    //printf("%d\n",xlength);
+    
+    collideField = (double*) malloc(Q*(xlength+2)*(xlength+2)*(xlength+2)*sizeof(double));
+    streamField = (double*) malloc(Q*(xlength+2)*(xlength+2)*(xlength+2)*sizeof(double));
+    flagField = (int*) malloc((xlength+2)*(xlength+2)*(xlength+2)*sizeof(int));
 
+    initialiseFields(collideField, streamField, flagField, xlength);
 
-  readParameters(&xlength, &tau, &velocityWall, &timesteps, &timestepsPerPlotting, argc, argv[1]);
-  //printf("%d\n",xlength);
+  for (int x =0;x<(xlength+2);x++)
+     {
+       for (int y =0;y<(xlength+2);y++)
+         {
+            for (int z =0;z<(xlength+2);z++)
+             {
+                 for( int i =0 ; i<Q ; i++)
+                  {
+                    printf("%f",collideField[Q*(z*(xlength+2)*(xlength+2) + y*(xlength+2) + x) + i]);
+                  }
+            
+             }
+         printf("\n");
+         }
+      printf("\n");
+      }
 
-
+    free(collideField);
+    free (streamField);
+    free (flagField);
   return 0;
 }
 
 #endif
+
+
+
+
+
+
 
