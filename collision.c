ced28d58c2df79744b23353f98361f7ca82f7920
Apoorv Gupta <apoorva.gupta@tum.de>
2016-05-12 23:54:15 +0200

    collision.c

--- collision.c
+++ collision.c
@@ -5,6 +5,25 @@ void computePostCollisionDistributions(double *currentCell, const double * const
 }
 
 void doCollision(double *collideField, int *flagField,const double * const tau,int xlength){
-  /* TODO */
+  double density;
+  double velocity[3];
+  double feq[Q];
+  int currentCell[3];
+  
+  for (int z = 1; z<=xlength; z++)
+      {
+        for (int y = 1; y<=xlength; y++)
+          {
+            for(int x = 1; x<=xlength; x++)
+               {
+                  currentCell = {z,y,x};
+                  computeDensity (currentCell,&density);
+                  computeVelocity(currentCell,&density,velocity);
+                  computeFeq(density,velocity,feq);
+                  computePostCollisionDistributions(currentCell, tau, feq);
+               }
+           }
+        }
+
 }
 
